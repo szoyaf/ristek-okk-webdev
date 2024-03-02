dcccf1fbaab5b91db725dc6f5e262bddc65c8668
szoyaf <shaneyzoyaf@gmail.com>
2024-03-02 21:14:27 +0700

    add db migration

---
+++ demo/src/main/resources/db/migration/ristek/V1__init_script.sql
@@ -0,0 +1,44 @@
+INSERT INTO fakultas (name) VALUES('Fakultas Ilmu Komputer');
+INSERT INTO fakultas (name) VALUES('Fakultas Ekonomi');
+INSERT INTO fakultas (name) VALUES('Fakultas Hukum');
+INSERT INTO fakultas (name) VALUES('Fakultas Ilmu Budaya');
+
+INSERT INTO tipe_panitia (na me) VALUES('Panitia Inti');
+INSERT INTO tipe_panitia (name) VALUES('Badan Pengurus Harian');
+
+INSERT INTO posisi_pengurus_inti (name) VALUES('Project Officer');
+INSERT INTO posisi_pengurus_inti (name) VALUES('Vice Project Officer Internal');
+INSERT INTO posisi_pengurus_inti (name) VALUES('Vice Project Officer Eksternal');
+INSERT INTO posisi_pengurus_inti (name) VALUES('Sekretaris Umum');
+INSERT INTO posisi_pengurus_inti (name) VALUES('Controller');
+INSERT INTO posisi_pengurus_inti (name) VALUES('Treasurer');
+INSERT INTO posisi_pengurus_inti (name) VALUES('Koordinator bidang Acara');
+INSERT INTO posisi_pengurus_inti (name) VALUES('Sarana dan Prasarana');
+INSERT INTO posisi_pengurus_inti (name) VALUES('Operasional');
+INSERT INTO posisi_pengurus_inti (name) VALUES('Materi dan Mentor');
+INSERT INTO posisi_pengurus_inti (name) VALUES('Kreatif');
+INSERT INTO posisi_pengurus_inti (name) VALUES('Relasi');
+
+INSERT INTO divisi_bph (name) VALUES('Project');
+INSERT INTO divisi_bph (name) VALUES('Sponsorship');
+INSERT INTO divisi_bph (name) VALUES('Kesekretariatan');
+INSERT INTO divisi_bph (name) VALUES('PSDM');
+INSERT INTO divisi_bph (name) VALUES('Acara Puncak');
+INSERT INTO divisi_bph (name) VALUES('Eksplorasi');
+INSERT INTO divisi_bph (name) VALUES('Transportasi dan Konsumsi');
+INSERT INTO divisi_bph (name) VALUES('Perizinan');
+INSERT INTO divisi_bph (name) VALUES('Logistik');
+INSERT INTO divisi_bph (name) VALUES('Keamanan');
+INSERT INTO divisi_bph (name) VALUES('Medis');
+INSERT INTO divisi_bph (name) VALUES('Media Informasi');
+INSERT INTO divisi_bph (name) VALUES('Kelembagaan');
+INSERT INTO divisi_bph (name) VALUES('Materi');
+INSERT INTO divisi_bph (name) VALUES('Mentor');
+INSERT INTO divisi_bph (name) VALUES('Media Partner');
+INSERT INTO divisi_bph (name) VALUES('IT dan Broadcast');
+INSERT INTO divisi_bph (name) VALUES('Dekorasi dan Wardrobe');
+INSERT INTO divisi_bph (name) VALUES('Visual Design dan Dokumentasi');
+
+INSERT INTO posisi_bph(name) VALUES('Penanggung Jawab');
+INSERT INTO posisi_bph(name) VALUES('Wakil Penanggung Jawab');
+INSERT INTO posisi_bph(name) VALUES('Anggota');
